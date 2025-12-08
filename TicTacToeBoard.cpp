1789d773d3d43fde52c192025be447d8947af086
LinAugustus <linaugustusxuan@gmail.com>
2025-12-08 10:45:35 -0500

    Add files via upload

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -3,6 +3,12 @@
   #include <string>
   #include "TicTacToeBoard.hpp"
 
+
+  TicTacToeBoard::TicTacToeBoard() {
+    board= std::vector<std::vector<char>>(3, std::vector<char>(3, ' '));
+
+}
+
   
 //TODO: If givenSize is less than 3, create a 3x3 board.
 //Otherwise, initialize the board with the given size for both rows and columns.
